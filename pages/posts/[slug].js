1df19e2c459648c44e063a6808cb917c66f2a456
Rob Cipolla <vcipolla95@gmail.com>
2021-12-03 00:21:54 +0000

    bug fixing deployment

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -6,21 +6,26 @@ import Head from "next/head"
 
 export default function post({ data }) {
 
-    const { post } = data
+    if (!data) {
+        return (
+            <MainLayout>
+                <h1>Error</h1>
+            </MainLayout>
+        )
+    } else {
 
-    return (
-        <MainLayout>
-            <Head>
-                <title>{post.seo.metaTitle}</title>
-            </Head>
-            <div className="text-container">
-                <h1>{post.title}</h1>
-                <SanityBlockContent blocks={post.body} />
-                <h2>raw data</h2>
-                <JSONPretty style={{ maxWidth: "100ch", maxHeight: '500px', overflowX: "scroll", boxShadow: "0 0 5px rgba(0,0,0,.3" }} id="json-pretty" data={post}></JSONPretty>
-            </div>
-        </MainLayout>
-    )
+        const { post } = data
+
+        return (
+            <MainLayout>
+                <div className="text-container">
+                    <h1>{post.title}</h1>
+                    <h2>raw data</h2>
+                    <JSONPretty style={{ maxWidth: "100ch", maxHeight: '500px', overflowX: "scroll", boxShadow: "0 0 5px rgba(0,0,0,.3" }} id="json-pretty" data={post}></JSONPretty>
+                </div>
+            </MainLayout>
+        )
+    }
 }
 
 export async function getStaticProps({ params }) {
